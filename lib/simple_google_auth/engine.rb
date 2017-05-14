2bc14ae49db4646ae79f6e86f58c2be5502ba921
Ben Anderson <benanderson@acidic.co.nz>
2017-05-14 12:41:06 +1200

    Add support for Rails 5 API mode

--- lib/simple_google_auth/engine.rb
+++ lib/simple_google_auth/engine.rb
@@ -3,6 +3,8 @@ module SimpleGoogleAuth
     initializer "simple_google_auth.load_helpers" do
       ActionController::Base.send :include, SimpleGoogleAuth::Controller
       ActionController::Base.send :helper_method, :google_auth_data
+
+      ActionController::API.send :include, SimpleGoogleAuth::Controller
     end
   end
 end
